358b0e75680da86dc771aaf86d9745aee42a4e55
pwnyaka <vas7424@mail.ru>
2021-03-28 16:43:27 +0400

    index

--- src/index.js
+++ src/index.js
@@ -7,29 +7,30 @@ const messages = ["Hello"]
 
 const Messages = () => {
   return (
-    <div>
-      <h1>messages</h1>
-      {messages.map((message, key) => (
-        <p key={key}>{message}</p>
-      ))}
-      <input placeholder="Введите сообщение" />
-      <button onClick={(e) => {
-        e.preventDefault()
-        messages.push(document.querySelector('input').value)
-        ReactDOM.render(
-            <>
-              <Messages title="title" />
-            </>,
-            document.querySelector("#root"),
-        )
-      }}>Отправить</button>
-    </div>
+      <div>
+        <h1>messages</h1>
+        {messages.map((message, key) => (
+            <p key={key}>{message}</p>
+        ))}
+        <input placeholder="Введите сообщение"/>
+        <button onClick={(e) => {
+          e.preventDefault()
+          messages.push(document.querySelector('input').value)
+          ReactDOM.render(
+              <>
+                <Messages title="title"/>
+              </>,
+              document.querySelector("#root"),
+          )
+        }}>Отправить
+        </button>
+      </div>
   )
 }
 
 ReactDOM.render(
-  <>
-    <Messages title="title" />
-  </>,
-  document.querySelector("#root"),
+    <>
+      <Messages title="title"/>
+    </>,
+    document.querySelector("#root"),
 )
